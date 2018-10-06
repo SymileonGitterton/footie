8bef05a3c3c3cf66bbe4323275034175f2e0328f
Peter Arnold <peter_arnold@TouchyMax.local>
2018-10-06 11:44:04 -0700

    ready for simulations

--- footie.js
+++ footie.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const CREST_HEIGHT = 30;
+
 const CLUBS_IN_LEAGUE = 20;
 const RELEGATION_THRESOLD = 18;
 
@@ -31,6 +33,7 @@ let leagueClubs = {};
 let leagueTable = [];
 let incomingObjectTeams = {};
 let incomingObjectMatches = {};
+let matchGrid = [];
 
 // football-data.org credential
 let footballHeaders = new Headers();
@@ -40,9 +43,10 @@ footballHeaders.append("X-Auth-Token", "102c4a1fda584443861d8e3f4fe4096e");
 
 let constructTheTable = function() {
   for(let i=0;i<CLUBS_IN_LEAGUE;i++) {
-    leagueTable[i] = {"position":0, "club":-1, "points":0};    // no club assigned to this position
+    leagueTable[i] = {"position":0, "club":-1};    // no club assigned to this position
   }
 };
+
 
 let constructTheChart = function() {
   // first, the header row
@@ -61,7 +65,7 @@ let constructTheChart = function() {
     // start a new row
     let newTrNode  = document.createElement("tr");
 
-    // always populate the 'position' colyoum
+    // always populate the 'position' colyume
     let newTdNode   = document.createElement("td");
     let newTextNode = document.createTextNode(leagueTable[row].position);
     newTdNode.appendChild(newTextNode);
@@ -71,8 +75,7 @@ let constructTheChart = function() {
     newTdNode = document.createElement("td");
     let newImgNode = document.createElement("img");
     newImgNode.src = "EPL_logo.png";
-    newImgNode.height = "35";
-    //newImgNode.width = "35";
+    newImgNode.height = CREST_HEIGHT;
     newTdNode.appendChild(newImgNode);
     newTrNode.appendChild(newTdNode);
 
@@ -93,28 +96,22 @@ let constructTheChart = function() {
 let populateTheChart = function() {
   let myTableBody = document.getElementById("myFootieTableBody");
   let myTableRows = myTableBody.getElementsByTagName("tr");
-  console.log(myTableRows);
-  console.log("length is "+myTableRows.length);
   for (let row=0; row<leagueTable.length; row++) {  
     let currentRow = myTableRows[row];
     let myRowCells = currentRow.getElementsByTagName("td");
+
     // put position in col 0
-    //console.log("row "+row);
-    //console.log("cell 0 contains "+myRowCells[0].innerHTML+", replacing with "+leagueTable[row].position+"\n");
     myRowCells[0].innerHTML = leagueTable[row].position;    
     if (leagueClubs.hasOwnProperty(leagueTable[row].club)) {
       let currentClub = leagueClubs[leagueTable[row].club];
       
       // do the crest logo in cell 1
-      console.log(myRowCells[1]);
       let newTdNode = document.createElement("td");
       let newImgNode = document.createElement("img");
       newImgNode.src = currentClub.crest;
-      newImgNode.height = "35";
-      //newImgNode.width = "35";
+      newImgNode.height = CREST_HEIGHT;
       newTdNode.appendChild(newImgNode);
       myRowCells[1].replaceWith(newTdNode);
-      console.log(myRowCells[1]);
       
       // do the rest, columns 2-n
       for (let column=2;column<tableHeaderNames.length;column++) {
@@ -123,48 +120,20 @@ let populateTheChart = function() {
         newTdNode.appendChild(newTextNode);
         myRowCells[column].replaceWith(newTdNode);
       }
-
     }
   }
 };
-    /*
-  // col 0
 
-    
-    // cols 1-n
-    for(let i=1; i<leagueClubs.lengt; i++) {  
-      let newTdNode = document.createElement("td");
-    let textNode1 = document.createTextNode(leagueClubs[team].name);
-    newTdNode1.appendChild(textNode1);
-    
-    // col 2 points  
-    let newTdNode2 = document.createElement("td");
-    let textNode2 = document.createTextNode(leagueClubs[team].points); 
-    newTdNode2.appendChild(textNode2);
-      
-    // col 3 
-    let newTdNode3 = document.createElement("td");
-      let textNode3 = document.createTextNode(team); 
-      newTdNode3.appendChild(textNode3);
 
-      let newRowNode = document.createElement("tr");
-      newRowNode.appendChild(newTdNode0);
-      newRowNode.appendChild(newTdNode1);
-      newRowNode.appendChild(newTdNode2);
-      newRowNode.appendChild(newTdNode3);
-
-      document.getElementById("myFootieTableBody").appendChild(newRowNode);
-    }
-  }
-};
-*/
-
+//=================================================
+// begin execution here
+//=================================================
 
 constructTheTable();
-console.log(leagueTable);
 constructTheChart();
 
 // construct dictionary object of club objects
+// and build the 20x20 match grid
 fetch('https://api.football-data.org/v2/competitions/PL/teams', {
 	      method: "GET",
         cache: "no-cache",
@@ -175,9 +144,10 @@ fetch('https://api.football-data.org/v2/competitions/PL/teams', {
   })
   .then(function(myJson) {
     incomingObjectTeams = JSON.parse(JSON.stringify(myJson));
-    console.log(incomingObjectTeams);
     for (let team=0; team<incomingObjectTeams.teams.length;team++) {
       let thisClubId = incomingObjectTeams.teams[team]["id"];
+      let thisClubResults = [];
+
       let thisClub = {  "name":         incomingObjectTeams.teams[team]["shortName"],
                         "crest":        incomingObjectTeams.teams[team]["crestUrl"],
                         "played":       0,
@@ -192,16 +162,16 @@ fetch('https://api.football-data.org/v2/competitions/PL/teams', {
                         "potentialPointsBest":  MAX_POINTS_AVAILABLE,
                         "potentialFinishWorst": CLUBS_IN_LEAGUE,
                         "potentialFinishBest": 1,
-                        "position":1999,
+                        "position": 0,
+                        "results": thisClubResults,
       };
-      leagueClubs[thisClubId] = thisClub;
+      leagueClubs[thisClubId] = thisClub;       // add the club to the clubs collection
+      let homeClubFixtures = [];
+      matchGrid[thisClubId] = homeClubFixtures;  // add a row to the results grid
     }
-    console.log(leagueClubs);
-    console.log("longness = ",Object.keys(leagueClubs).length);
-    //populateTheChart();
   })
   .then(function() {
-    // now unwrap the matches and tally points
+    // now collect the match data
     fetch('https://api.football-data.org/v2/competitions/PL/matches', {
         method: "GET",
         cache: "no-cache",
@@ -213,42 +183,99 @@ fetch('https://api.football-data.org/v2/competitions/PL/teams', {
   .then(function(myJson) {
     incomingObjectMatches = JSON.parse(JSON.stringify(myJson));
     console.log(incomingObjectMatches);
-    
-    // calculate points per club by parsing all match records
+
+    // calculate goals and points per club by parsing all match records
     for (let i=0; i<incomingObjectMatches.matches.length; i++) {
       let thisMatch = incomingObjectMatches.matches[i];
-      //console.log("thisMatch is",thisMatch);
-      let opstring = i+": "+thisMatch.homeTeam.name+" "+thisMatch.score.fullTime.homeTeam+"-"+thisMatch.score.fullTime.awayTeam+" "+thisMatch.awayTeam.name;
-      //console.log(i,thisMatch.homeTeam.name,thisMatch.score.fullTime.homeTeam,"-",thisMatch.score.fullTime.awayTeam,thisMatch.awayTeam.name);
-      //console.log(opstring);
+      if (thisMatch.status === "FINISHED") {
+        let home = leagueClubs[thisMatch.homeTeam.id];
+        let away = leagueClubs[thisMatch.awayTeam.id];
+        home.played += 1;
+        away.played += 1;
+        if (thisMatch.score.duration === "REGULAR") {
+          home.goalsFor      += thisMatch.score.fullTime.homeTeam;
+          home.goalsAgainst  += thisMatch.score.fullTime.awayTeam;
+          away.goalsFor      += thisMatch.score.fullTime.awayTeam;
+          away.goalsAgainst  += thisMatch.score.fullTime.homeTeam;
+        }
+        switch (thisMatch.score.winner) {
+          case "HOME_TEAM":
+            home.points += POINTS_PER_WIN;
+            away.points += POINTS_PER_LOSS;
+            home.results[away] = POINTS_PER_WIN;
+            away.results[home] = POINTS_PER_LOSS;
+            home.won += 1;
+            away.lost += 1;
+            break;
+          case "AWAY_TEAM":
+            home.points += POINTS_PER_LOSS;
+            away.points += POINTS_PER_WIN;
+            home.results[away] = POINTS_PER_LOSS;
+            away.results[home] = POINTS_PER_WIN;
+            home.lost += 1;
+            away.won += 1;
+            break;
+          case "DRAW":
+            home.points += POINTS_PER_DRAW;
+            away.points += POINTS_PER_DRAW;
+            home.results[away] = POINTS_PER_DRAW;
+            away.results[home] = POINTS_PER_DRAW;
+            home.drawn += 1;
+            away.drawn += 1;
+            break;
+          default:
+            break;
+        }
+        home.goalDifference = home.goalsFor-home.goalsAgainst;
+        away.goalDifference = away.goalsFor-away.goalsAgainst;
+      }
+    }
+    console.log("done collecing points etc.\n");
+
+    // calculate max possible points - easy
+
+
+
+
+    // insert the teams into the table in object order (meaningless), taking data from club record
+    let tableRow=0;
+    for(let club in leagueClubs) {
+      if (tableRow < leagueTable.length) {
+        leagueTable[tableRow].position  = parseInt(leagueClubs[club].position,10)-parseInt(club,10);
+        leagueTable[tableRow].club      = parseInt(club,10);
+        leagueTable[tableRow].points    = leagueClubs[club].points;
+        tableRow++;
+      } else {
+        console.log("table row overflow\n");
+      }
+    }
+    console.log("should be unsorted:");
+    let logTable = JSON.parse(JSON.stringify(leagueTable)); // SERDES to take deep-copy snapshot
+    console.log(logTable);  // otherwise console.log just queues the array, and later prints its final state 
+
+    // then sort the teams in the table by points
+    leagueTable.sort(function(a,b) { 
+      if (leagueClubs[b.club].points !== leagueClubs[a.club].points) {
+        return (leagueClubs[b.club].points - leagueClubs[a.club].points)
+      } else {
+        if (leagueClubs[b.club].goalDifference !== leagueClubs[a.club].goalDifference) {
+          return (leagueClubs[b.club].goalDifference - leagueClubs[a.club].goalDifference)
+        } else {
+            return (leagueClubs[b.club].goalsFor - leagueClubs[a.club].goalsFor);
+          }
+        }
+      });
+
+    for (let i=0;i<leagueTable.length;i++) {
+      leagueTable[i].position = i+1;
     }
 
-  // calculate each club's position and store in the record
+    // should sort out case wen teams are truly equal through all tie-breakers
 
-  // insert the teams into the table in object order (meaningless)
-  console.log("initial commit\n");
-  console.log(leagueTable);
-  let tableRow=0;
-  for(let club in leagueClubs) {
-    if (tableRow < leagueTable.length) {
-      leagueTable[tableRow].position  = parseInt(leagueClubs[club].position,10)-parseInt(club,10);
-      leagueTable[tableRow].club      = parseInt(club,10);
-      leagueTable[tableRow].points    = leagueClubs[club].points;
-      tableRow++;
-    } else {
-      console.log("table row overflow\n");
-    }
-  }
-  console.log(leagueTable);
+    // now show it
+    populateTheChart();
 
-  // sort the teams in the table and assign positions
-  console.log("now sort\n")
-  //leagueTable[i] = {"position":0, "club":-1, "points":0};    // no club assigned to this position
-  
-  // now show it
-  populateTheChart();
-
+    });
   });
-});
 
 
